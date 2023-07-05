b8203ded212616aad1fa36a71e9771a8450e4cba
luisantos1986 <ing.luisantos@gmail.com>
2023-07-05 14:52:00 -0600

    swagger update

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimit = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 
@@ -35,7 +40,12 @@ app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(xss())
-// extra packages
+
+app.get('/', (req, res) => {
+  res.send('<h1>JOB API</h1><a href="/api-docs">documentation</a>')
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter)
